af2cd15764de473b502a49b38be5ce180f11e6e8
swateja <swateja@gmail.com>
2024-02-19 22:16:44 +0530

    changed the helloworld.py code

--- src/main/python/helloworld.py
+++ src/main/python/helloworld.py
@@ -1,4 +1,13 @@
 import sys
 
 def helloworld(out):
+    """
+    Print a hello world message.
+
+    Args:
+        out: Output stream to write the message to.
+    """
     out.write("Hello world of Python\n")
+
+# Call the function with a file object
+helloworld(sys.stdout)
